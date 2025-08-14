445f4fa5464071187e05ec5e06c79137f641d8be
Aas MOhammad <aasmohd7071@gmail.com>
2025-08-14 23:56:06 +0530

    intial commit

---
+++ src/models/homeModel.js
@@ -0,0 +1,155 @@
+const liveMathces =  [
+    {
+        id: "d54c93cc-abd1-44c7-a534-ede33788e69e",
+        name: "singapure vs some thing else, 1st T20I",
+        matchType: "t20",
+        status: "Singapore Women won by 125 runs",
+        venue: "Singapore National Cricket Ground, Singapore",
+        dateTimeGMT: "2025-08-13T04:30:00",
+        teams: [
+            "pakistan",
+            "West indease"
+        ],
+        teamInfo: [
+            {
+                name: "Cambodia Women",
+                shortname: "CBW",
+                img: "https://h.cricapi.com/img/icon512.png"
+            },
+            {
+                name: "Singapore Women",
+                shortname: "SIN-W",
+                img: "https://g.cricapi.com/iapi/1553-637926305464374356.webp?w=48"
+            }
+        ],
+        score: [
+            { team: "Pakistan", score: "12/2 (2.4)" },
+            { team: "West Indies", score: "45/1 (5.0)" }
+        ],
+
+
+        matchStarted: true,
+        matchEnded: true
+    },
+
+
+    {
+        id: "m1",
+        name: "India vs Pakistan, 1st T20I",
+        matchType: "t20",
+        status: "India won by 5 wickets",
+        venue: "Wankhede Stadium, Mumbai",
+        dateTimeGMT: "2025-08-10T14:00:00",
+        teams: ["India", "Pakistan"],
+        teamInfo: [
+            { name: "India", shortname: "IND", img: "https://h.cricapi.com/img/icon512.png" },
+            { name: "Pakistan", shortname: "PAK", img: "https://h.cricapi.com/img/icon512.png" }
+        ],
+        score: [
+            { team: "India", score: "165/5 (18.2)" },
+            { team: "Pakistan", score: "162/7 (20.0)" }
+        ],
+        matchStarted: true,
+        matchEnded: true
+    },
+    {
+        id: "m2",
+        name: "Bangladesh vs Australia, 2nd T20I",
+        matchType: "t20",
+        status: "Australia won by 20 runs",
+        venue: "Sher-e-Bangla National Stadium, Dhaka",
+        dateTimeGMT: "2025-08-11T10:00:00",
+        teams: ["Bangladesh", "Australia"],
+        teamInfo: [
+            { name: "Bangladesh", shortname: "BAN", img: "https://h.cricapi.com/img/icon512.png" },
+            { name: "Australia", shortname: "AUS", img: "https://h.cricapi.com/img/icon512.png" }
+        ],
+        score: [
+            { team: "Bangladesh", score: "145/9 (20.0)" },
+            { team: "Australia", score: "165/6 (20.0)" }
+        ],
+        matchStarted: true,
+        matchEnded: true
+    },
+    {
+        id: "m3",
+        name: "England vs South Africa, 3rd T20I",
+        matchType: "t20",
+        status: "England won by 3 wickets",
+        venue: "Old Trafford, Manchester",
+        dateTimeGMT: "2025-08-12T17:00:00",
+        teams: ["England", "South Africa"],
+        teamInfo: [
+            { name: "England", shortname: "ENG", img: "https://h.cricapi.com/img/icon512.png" },
+            { name: "South Africa", shortname: "SA", img: "https://h.cricapi.com/img/icon512.png" }
+        ],
+        score: [
+            { team: "England", score: "158/7 (19.4)" },
+            { team: "South Africa", score: "156/8 (20.0)" }
+        ],
+        matchStarted: true,
+        matchEnded: true
+    },
+    {
+        id: "m4",
+        name: "Sri Lanka vs New Zealand, 1st T20I",
+        matchType: "t20",
+        status: "New Zealand won by 8 wickets",
+        venue: "Pallekele International Cricket Stadium, Kandy",
+        dateTimeGMT: "2025-08-13T13:30:00",
+        teams: ["Sri Lanka", "New Zealand"],
+        teamInfo: [
+            { name: "Sri Lanka", shortname: "SL", img: "https://h.cricapi.com/img/icon512.png" },
+            { name: "New Zealand", shortname: "NZ", img: "https://h.cricapi.com/img/icon512.png" }
+        ],
+        score: [
+            { team: "Sri Lanka", score: "142/6 (20.0)" },
+            { team: "New Zealand", score: "143/2 (17.5)" }
+        ],
+        matchStarted: true,
+        matchEnded: true
+    },
+    {
+        id: "m5",
+        name: "West Indies vs Afghanistan, 2nd T20I",
+        matchType: "t20",
+        status: "West Indies won by 35 runs",
+        venue: "Kensington Oval, Bridgetown",
+        dateTimeGMT: "2025-08-14T18:00:00",
+        teams: ["West Indies", "Afghanistan"],
+        teamInfo: [
+            { name: "West Indies", shortname: "WI", img: "https://h.cricapi.com/img/icon512.png" },
+            { name: "Afghanistan", shortname: "AFG", img: "https://h.cricapi.com/img/icon512.png" }
+        ],
+        score: [
+            { team: "West Indies", score: "180/4 (20.0)" },
+            { team: "Afghanistan", score: "145/8 (20.0)" }
+        ],
+        matchStarted: true,
+        matchEnded: true
+    },
+    {
+        id: "m6",
+        name: "Zimbabwe vs Ireland, 3rd T20I",
+        matchType: "t20",
+        status: "Ireland won by 6 wickets",
+        venue: "Harare Sports Club, Harare",
+        dateTimeGMT: "2025-08-15T12:00:00",
+        teams: ["Zimbabwe", "Ireland"],
+        teamInfo: [
+            { name: "Zimbabwe", shortname: "ZIM", img: "https://h.cricapi.com/img/icon512.png" },
+            { name: "Ireland", shortname: "IRE", img: "https://h.cricapi.com/img/icon512.png" }
+        ],
+        score: [
+            { team: "Zimbabwe", score: "150/7 (20.0)" },
+            { team: "Ireland", score: "151/4 (18.3)" }
+        ],
+        matchStarted: true,
+        matchEnded: true
+    }
+
+
+
+]
+
+export default liveMathces
---
+++ src/models/playerModel.js
@@ -0,0 +1,11 @@
+const playerInfo = {
+    id: "7ac9bb40-8577-4214-a070-81920d745d09",
+    name: "Lucas Barker",
+    dateOfBirth: "1995-03-24T00:00:00",
+    role: "Batsman",
+    battingStyle: "Right Handed Bat",
+    bowlingStyle: "Right-arm medium",
+    placeOfBirth: "Colne, Lancashire",
+    country: "Guernsey",
+    playerImg: "https://h.cricapi.com/img/icon512.png"
+}
